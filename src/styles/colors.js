fae22cba2db8dc921b696af02d561ae0d0ad43b2
ElMayo <mic.l.meier@gmail.com>
2020-10-24 15:11:20 +0200

    style: Add colors to 'styles/colors.js'

--- src/styles/colors.js
+++ src/styles/colors.js
@@ -1,1 +1,7 @@
+export const base = 'white'
+export const text = '#434449'
+export const gray = '#f1f2f7'
+export const gray10 = '#f1f1f4'
+export const gray20 = '#e4e5e9'
+export const gray80 = '#6f7077'
 export const danger = '#ef5350'
