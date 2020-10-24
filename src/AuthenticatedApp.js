63663afe7207258b5c5ba36f5137a1d32133f71f
ElMayo <mic.l.meier@gmail.com>
2020-10-24 20:51:12 +0200

    feat: Add functionality to add and retreive data from firestore
    Basic testing of the firestore interaction

--- src/AuthenticatedApp.js
+++ src/AuthenticatedApp.js
@@ -4,9 +4,11 @@ import { jsx } from '@emotion/core'
 import React from 'react'
 
 import { Button } from './components/lib'
+import fb from './firebase'
+
+const db = fb.firestore()
 
 function Nav({ handleLogout, user }) {
-  console.log('user', user)
   return (
     <nav
       css={{
@@ -28,9 +30,51 @@ function Nav({ handleLogout, user }) {
 }
 
 function AuthenticatedApp({ handleLogout, user }) {
+  const [newTodo, setNewTodo] = React.useState('')
+  const [todos, setTodos] = React.useState(null)
+
+  React.useEffect(() => {
+    db.collection(user.uid)
+      .get()
+      .then((querySnapshot) => {
+        const docsArr = []
+        querySnapshot.forEach((doc) => {
+          docsArr.push({ data: doc.data(), id: doc.id })
+        })
+        setTodos(docsArr)
+      })
+      .catch((e) => console.log('Error: ', e))
+  }, [])
+
+  function handleSubmit() {
+    db.collection(user.uid)
+      .add({
+        task: newTodo,
+        completed: false,
+      })
+      .then((docRef) => {
+        console.log('Document written with ID: ', docRef.id)
+      })
+      .catch((error) => {
+        console.error('Error adding document: ', error)
+      })
+    setNewTodo('')
+  }
+
   return (
     <React.Fragment>
       <Nav handleLogout={handleLogout} user={user} />
+      <input
+        type="text"
+        value={newTodo}
+        onChange={(e) => setNewTodo(e.target.value)}
+      />
+      <Button variant="primary" onClick={handleSubmit}>
+        Submit
+      </Button>
+      {todos
+        ? todos.map((todo) => <div key={todo.id}>{todo.data.task}</div>)
+        : null}
     </React.Fragment>
   )
 }
